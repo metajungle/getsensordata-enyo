223ffa773fa954d4a2501be8ecc9b020d3d5b86e
Jakob Henriksson <jakob@metajungle.net>
2013-06-07 19:59:01 -0400

    fixes

--- source/App.js
+++ source/App.js
@@ -1,17 +1,42 @@
 enyo.kind({
-	name: "App",
-	kind: "FittableRows",
-	fit: true,
-	components:[
-		{kind: "onyx.Toolbar", content: "Hello World"},
-		{kind: "enyo.Scroller", fit: true, components: [
-			{name: "main", classes: "nice-padding", allowHtml: true}
-		]},
-		{kind: "onyx.Toolbar", components: [
-			{kind: "onyx.Button", content: "Tap me", ontap: "helloWorldTap"}
-		]}
-	],
-	helloWorldTap: function(inSender, inEvent) {
-		this.$.main.addContent("The button was tapped.<br/>");
-	}
+    name: "App",
+    kind: "FittableRows",
+    fit: true,
+    components: [{
+        kind: "onyx.Toolbar",
+        content: "getsensordata.net"
+    }, {
+        kind: "Panels",
+        name: "main",
+        fit: true,
+        classes: "panels-sample-sliding-panels",
+        arrangerKind: "CollapsingArranger",
+        wrap: false,
+        components: [{
+            content: "Test",
+            tag: 'p', 
+        }, {
+            kind: "MapView",
+            fit: true, 
+        }]
+    }], 
+    setupItem: function(inSender, inEvent) {
+        this.$[inSender.item].setContent("This is row number: " + inEvent.index);
+    },
+    helloWorldTap: function(inSender, inEvent) {
+        //this.$.main.addContent("The button was tapped.<br/>");
+        alert('hello');
+    },
+    togglePane: function(inSender, inEvent) {
+        map.invalidateSize();
+        this.$.main.setIndex(this.$.main.index ? 0 : 1);
+        this.$.contentPanels.setIndex(this.$.contentPanels.index ? 0 : 1);
+    },
+    toggleServices: function(sender, event) {
+
+    },
+    toggleTime: function(sender, event) {
+
+    },
+
 });
