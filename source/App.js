0775068e64d6a2030d01f5f6f8fd1d334f934020
Jakob Henriksson <jakob@metajungle.net>
2013-06-07 19:49:32 -0400

    minor updates

--- source/App.js
+++ source/App.js
@@ -2,71 +2,68 @@ enyo.kind({
     name: "App",
     kind: "Control",
     components: [{
-        kind: "Panels",
-	name: "main",
-	classes: "panels-sample-sliding-panels enyo-fit", 
-	arrangerKind: "CollapsingArranger", 
-	wrap: false,
-	components: [
-	    {
-		kind: "FittableRows", 
-		name: "side", 
-		classes: "enyo-fit", 
-		components: [
-		    {
-			kind: "onyx.Toolbar",
-			components: [{
-			    kind: "onyx.Grabber"
-			}], 
-		    }, 
-		    {
-			contents: "Hello", 
-			classes: "enyo-fit", 
-			tag: "p", 
-			fit: true
-		    }
-		]
-	    },
-	    {
-		kind: "FittableRows", 
-		name: "map-main", 
-		components: [
-		    {
-			kind: "onyx.Toolbar",
-			components: [{
-			    kind: "onyx.Grabber"
-			}], 
-		    }, 
-		    {
-			kind: "MapView", 
-			fit: true
-		    }
-		]
-	    }
-	]
-    }],
+        kind: "FittableRows",
+        name: "map",
+        fit: true, 
+        classes: "enyo-fit",
+        components: [{
+            kind: "onyx.Toolbar",
+            components: [{
+                content: "getsensordata.net", 
+                tag: "p"
+            }, 
+            {
+                name: "menu",
+                defaultKind: "onyx.Button",
+                components: [{
+                    content: "A",
+                    panel: "info",
+                    ontap: "togglePullout"
+                }, {
+                    content: "B",
+                    panel: "bookmark",
+                    ontap: "togglePullout"
+                }, ]
+            }, {
+                kind: "onyx.Button",
+                src: "assets/images/menu-icon-bookmark.png",
+                active: true,
+                ontap: "togglePullout",
+                panel: "info", 
+                classes: "onyx-affirmative menu",
+                content: "M"
+            }, ]
+        }, {
+            kind: "MapView",
+            fit: true
+        }, ]
+    }, {
+        kind: "Pullout",
+        classes: "pullout",
+        components: []
+    }, ],
     setupItem: function(inSender, inEvent) {
-	this.$[inSender.item].setContent("This is row number: " + inEvent.index);
+        this.$[inSender.item].setContent("This is row number: " + inEvent.index);
     },
     helloWorldTap: function(inSender, inEvent) {
-	//this.$.main.addContent("The button was tapped.<br/>");
-	alert('hello');
+        //this.$.main.addContent("The button was tapped.<br/>");
+        alert('hello');
     },
     togglePane: function(inSender, inEvent) {
-	this.$.pullout.toggle(inSender.panel);
-	//enyo.$.app_pullout.show();
-	//map.invalidateSize();
-	//this.$.main.setIndex(this.$.main.index ? 0 : 1); 
-	// this.$.contentPanels.setIndex(this.$.contentPanels.index ? 0 : 1);
+        this.$.pullout.toggle(inSender.panel);
+        //enyo.$.app_pullout.show();
+        //map.invalidateSize();
+        //this.$.main.setIndex(this.$.main.index ? 0 : 1); 
+        // this.$.contentPanels.setIndex(this.$.contentPanels.index ? 0 : 1);
     },
     togglePullout: function(inSender) {
-	this.$.pullout.toggle(inSender.panel);
+        this.$.pullout.toggle(inSender.panel);
     },
     toggleServices: function(sender, event) {
-	
+
     },
     toggleTime: function(sender, event) {
-	
+
     },
-    
+
 });
