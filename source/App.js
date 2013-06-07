bd7e3108dd9273eddd8d4aedaa345d1a08f23e29
Jakob Henriksson <jakob@metajungle.net>
2013-06-07 14:23:37 -0400

    sliding panes

--- source/App.js
+++ source/App.js
@@ -1,17 +1,72 @@
 enyo.kind({
-	name: "App",
-	kind: "FittableRows",
-	fit: true,
-	components:[
-		{kind: "onyx.Toolbar", content: "Hello World"},
-		{kind: "enyo.Scroller", fit: true, components: [
-			{name: "main", classes: "nice-padding", allowHtml: true}
-		]},
-		{kind: "onyx.Toolbar", components: [
-			{kind: "onyx.Button", content: "Tap me", ontap: "helloWorldTap"}
-		]}
-	],
-	helloWorldTap: function(inSender, inEvent) {
-		this.$.main.addContent("The button was tapped.<br/>");
-	}
+    name: "App",
+    kind: "Control",
+    components: [{
+        kind: "Panels",
+	name: "main",
+	classes: "panels-sample-sliding-panels enyo-fit", 
+	arrangerKind: "CollapsingArranger", 
+	wrap: false,
+	components: [
+	    {
+		kind: "FittableRows", 
+		name: "side", 
+		classes: "enyo-fit", 
+		components: [
+		    {
+			kind: "onyx.Toolbar",
+			components: [{
+			    kind: "onyx.Grabber"
+			}], 
+		    }, 
+		    {
+			contents: "Hello", 
+			classes: "enyo-fit", 
+			tag: "p", 
+			fit: true
+		    }
+		]
+	    },
+	    {
+		kind: "FittableRows", 
+		name: "map-main", 
+		components: [
+		    {
+			kind: "onyx.Toolbar",
+			components: [{
+			    kind: "onyx.Grabber"
+			}], 
+		    }, 
+		    {
+			kind: "MapView", 
+			fit: true
+		    }
+		]
+	    }
+	]
+    }],
+    setupItem: function(inSender, inEvent) {
+	this.$[inSender.item].setContent("This is row number: " + inEvent.index);
+    },
+    helloWorldTap: function(inSender, inEvent) {
+	//this.$.main.addContent("The button was tapped.<br/>");
+	alert('hello');
+    },
+    togglePane: function(inSender, inEvent) {
+	this.$.pullout.toggle(inSender.panel);
+	//enyo.$.app_pullout.show();
+	//map.invalidateSize();
+	//this.$.main.setIndex(this.$.main.index ? 0 : 1); 
+	// this.$.contentPanels.setIndex(this.$.contentPanels.index ? 0 : 1);
+    },
+    togglePullout: function(inSender) {
+	this.$.pullout.toggle(inSender.panel);
+    },
+    toggleServices: function(sender, event) {
+	
+    },
+    toggleTime: function(sender, event) {
+	
+    },
+    
 });
